f37bba51c1f16d0f200a3dfe442ec8722af3a19f
Chris <christianotu.d@gmail.com>
2021-02-23 05:45:58 +0100

    Updated map component to be a presentational component, and adjusted map center

--- src/blocks/Map.tsx
+++ src/blocks/Map.tsx
@@ -1,20 +1,8 @@
 import React from "react";
 import L from "leaflet";
 import { MapContainer } from "react-leaflet";
-import { useSelector } from "react-redux";
-import { Tabs } from "../components/Tabs";
-import { getMapData } from "../selectors";
-
-const tabItems = [
-  "Cummulative Cases",
-  "Active Cases",
-  "Tests",
-  "Case-Fatality Ratio",
-];
 
 export const Map: React.FC = ({ children }) => {
-  const activeData = useSelector(getMapData);
-
   /*The farthest East longitude of a map is 180, West is -180.
   The farthest North Latitude of a map is 90, South is -90. 
   We use the farthest longitudes and latitudes to bound the map. This ensures we can't view anything outside these bounds.
@@ -26,19 +14,16 @@ export const Map: React.FC = ({ children }) => {
 
   return (
     <div>
-      <div id={activeData} role="tabpanel">
-        <MapContainer
-          center={bounds.getCenter()}
-          zoom={1}
-          minZoom={1}
-          zoomControl={false}
-          maxBounds={bounds}
-          maxBoundsViscosity={0.75}
-        >
-          {children}
-        </MapContainer>
-      </div>
-      <Tabs tabItems={tabItems} activeData={activeData} tabLabel="Map Data" />
+      <MapContainer
+        center={[20, 0]}
+        zoom={2}
+        minZoom={2}
+        zoomControl={false}
+        maxBounds={bounds}
+        maxBoundsViscosity={0.75}
+      >
+        {children}
+      </MapContainer>
     </div>
   );
 };
