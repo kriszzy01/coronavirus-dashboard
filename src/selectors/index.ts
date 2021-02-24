770f89d5b32bc34d882643c9d5b0b0908ae9c68c
Chris <christianotu.d@gmail.com>
2021-02-24 22:40:41 +0100

    Added selectors for updated redux slices

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,6 +1,7 @@
 import { RootState } from "../types";
 
-export const getCountries = (state: RootState) => state.statistics.countries;
-export const getProvince = (state: RootState) => state.statistics.province;
-export const getWorldwide = (state: RootState) => state.statistics.worldwide;
+export const getProvince = (state: RootState) => state.province.cummulative;
+export const getWorldwide = (state: RootState) => state.worldwide.cummulative;
+export const getCountry = (state: RootState) => state.country.cummulative;
 export const getMapData = (state: RootState) => state.mapData.activeData;
+export const getTargetData = (state: RootState) => state.mapData.targetData;
