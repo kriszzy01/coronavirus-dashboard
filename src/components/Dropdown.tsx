85c9a8bbace7f76393d7cd5b80b8236d21e8bba5
Chris <christianotu.d@gmail.com>
2021-02-25 08:06:13 +0100

    Updated dropdown component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Province } from "../types";
+import { setTargetData } from "../slices/map";
 
 interface Dropdown {
   searchResult: Province[];
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const Dropdown: React.FC<Dropdown> = ({ searchResult }) => {
-  const handleClick = (country: Province) => {
-    console.log(country);
+export const Dropdown: React.FC<Dropdown> = ({
+  searchResult,
+  searchTerm,
+  setSearchTerm,
+}) => {
+  const dispatch = useDispatch();
+  const isSearchResults = searchResult.length > 0;
+
+  const handleClick = (country: string) => {
+    dispatch(setTargetData(country));
+    setSearchTerm("");
   };
 
-  return (
-    <div
-      className="dropdown"
-      id="dropdown"
-      data-expanded={searchResult.length > 0 ? "true" : "false"}
-    >
-      <ul>
-        {searchResult.map((country) => {
-          const { flag, country: name } = country;
+  useEffect(() => {
+    //Stop scrolling on body if dropdown is open
+    if (searchTerm) {
+      document.querySelector("body")?.setAttribute("data-scroll", "");
+    }
 
-          return (
-            <li key={name} onClick={() => handleClick(country)}>
-              <span>
-                <img src={flag} alt={`flag of ${name}`} />
-              </span>
-              <span>{name}</span>
-            </li>
-          );
-        })}
-      </ul>
+    return () => {
+      document.querySelector("body")?.removeAttribute("data-scroll");
+    };
+  }, [searchTerm !== ""]);
+
+  return (
+    <div className="dropdown" id="dropdown" data-expanded={searchTerm !== ""}>
+      {isSearchResults ? (
+        <ul>
+          {searchResult.map(({ flag, country: name }) => {
+            return (
+              <li key={name} onClick={() => handleClick(name)}>
+                <span>
+                  <img src={flag} alt={`flag of ${name}`} />
+                </span>
+                <span>{name}</span>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>Country not found</p>
+      )}
     </div>
   );
 };
