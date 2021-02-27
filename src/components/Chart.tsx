4a178c4620a67ca05fbd7668ccc51b8b155e7463
Chris <christianotu.d@gmail.com>
2021-02-27 20:45:04 +0100

    Create initial chat component and styles

---
+++ src/components/Chart.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import C from "chart.js";
+import { ChartData } from "../types";
+import { chartOptions, commify } from "../utils";
+
+interface ChartProps {
+  type: string;
+  label: any[];
+  data: ChartData[];
+  id: string;
+  value?: number;
+}
+
+const colors: Record<string, string> = {
+  "Confirmed cases": "rgb(0, 147, 213)",
+  Deaths: "rgb(236, 0, 0)",
+  Recovered: "green",
+};
+
+export const Chart: React.FC<ChartProps> = ({ type, label, data, id }) => {
+  useEffect(() => {
+    const mychart = new C(id, {
+      type,
+      data: {
+        datasets: [
+          {
+            data,
+            backgroundColor: colors[label[1]],
+            hoverBackgroundColor: "rgb(2, 20, 34)",
+            borderColor: "rgba(32, 33, 36, 0.5)",
+            pointRadius: 0,
+          },
+        ],
+      },
+      options: chartOptions[type],
+    });
+
+    return () => {
+      mychart.destroy();
+    };
+  }, [type, data, id, label]);
+
+  return (
+    <div className="chart">
+      <p style={{ color: `${colors[label[1]]}` }}>
+        <span>{commify(label[0])}</span> <br />
+        <span>{label[1]}</span>
+      </p>
+      <div className="chart__container">
+        <canvas id={id}></canvas>
+      </div>
+    </div>
+  );
+};
