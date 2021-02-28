753f04b1389006e3d7361e35d8dede5421c5d3a9
Chris <christianotu.d@gmail.com>
2021-02-28 06:19:52 +0100

    Added paremeters to prevent duplicated fetch functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const getProvince = async () => {
-  const request_url = "https://disease.sh/v3/covid-19/jhucsse";
+export const getStatistics = async (parameter?: string) => {
+  const request_url = `https://disease.sh/v3/covid-19/${parameter || "all"}`;
 
   try {
     const response = await axios.get(request_url);
@@ -12,8 +12,10 @@ export const getProvince = async () => {
   }
 };
 
-export const getCountries = async () => {
-  const request_url = "https://disease.sh/v3/covid-19/countries";
+export const getHistorical = async (parameter?: string) => {
+  const request_url = `https://disease.sh/v3/covid-19/historical/${
+    parameter || "all"
+  }?lastdays=270`;
 
   try {
     const response = await axios.get(request_url);
@@ -23,29 +25,3 @@ export const getCountries = async () => {
     throw error;
   }
 };
-
-export const getWorldWide = async () => {
-  const request_url = "https://disease.sh/v3/covid-19/all";
-
-  try {
-    const response = await axios.get(request_url);
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getAllHistorical = async () => {
-  const request_url = "https://disease.sh/v3/covid-19/historical/all?lastdays=270";
-
-  try {
-    const response = await axios.get(request_url);
-
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getHistoricalData = async () => {};
