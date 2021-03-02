cd1eae7921cc1b1c55d5cbe78b460e61c309c81e
Chris <christianotu.d@gmail.com>
2021-03-02 06:18:19 +0100

    Added hook to stop scrolling depending on provided dependencies

---
+++ src/hooks/useNoScroll.ts
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+export const useNoScroll = (dependencies: any[]) => {
+  useEffect(() => {
+    //Stop scrolling on body if the first item on the provided dependencies is true
+    if (dependencies[0]) {
+      document.querySelector("body")?.setAttribute("data-scroll", "");
+    }
+
+    return () => {
+      document.querySelector("body")?.removeAttribute("data-scroll");
+    };
+  }, dependencies);
+};
