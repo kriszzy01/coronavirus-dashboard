d3a6b84394076085fea9d744139c146fa3b8275f
Chris <christianotu.d@gmail.com>
2021-03-07 20:48:24 +0100

    Updated to export all hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,25 +1,2 @@
-import { useState, useEffect } from "react";
-
-export const useDarkMode = (initialValue: boolean) => {
-  const [value, setValue] = useState(() => {
-    const userMode = window.matchMedia("(prefers-color-scheme:dark)").matches; //If user has darkmode set on system
-
-    if (userMode) {
-      return true;
-    }
-
-    let darkMode = window.localStorage.getItem("darkMode");
-
-    if (darkMode !== null) {
-      return Boolean(JSON.parse(darkMode));
-    }
-
-    return initialValue;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem("darkMode", JSON.stringify(value));
-  }, [value]);
-
-  return [value, setValue] as const;
-};
+export * from "./useSearchbar";
+export * from "./useNoScroll";
